e03e8c3db3438073924876ec33dd7f49f5e567b5
Paul Bowsher <paul.bowsher@gmail.com>
2017-05-19 20:12:56 +0100

    Don't encrypt secrets
    Zappa can’t handle them

--- projects/electionleaflets/app.tf
+++ projects/electionleaflets/app.tf
@@ -4,36 +4,30 @@ resource "aws_s3_bucket" "zappa_and_secrets" {
   region = "eu-west-1"
 }
 
-resource "aws_kms_key" "secrets_key" {
-  description = "Election Leaflets Zappa secrets key"
-}
-
 resource "aws_s3_bucket_object" "staging_secrets" {
-  bucket     = "${aws_s3_bucket.zappa_and_secrets.bucket}"
-  key        = "staging_secrets.json"
-  kms_key_id = "${aws_kms_key.secrets_key.arn}"
+  bucket = "${aws_s3_bucket.zappa_and_secrets.bucket}"
+  key    = "staging_secrets.json"
 
   content = <<JSON
 {
   "DATABASE_HOST": "${aws_db_instance.default.address}",
   "DATABASE_NAME": "${postgresql_database.electionleaflets_staging.name}",
   "DATABASE_USER": "${postgresql_role.electionleaflets_staging.name}",
-  "DATABASE_PASS": "${random_id.db_staging_password.b64}",
+  "DATABASE_PASS": "${random_id.db_staging_password.b64}"
 }
   JSON
 }
 
 resource "aws_s3_bucket_object" "production_secrets" {
-  bucket     = "${aws_s3_bucket.zappa_and_secrets.bucket}"
-  key        = "production_secrets.json"
-  kms_key_id = "${aws_kms_key.secrets_key.arn}"
+  bucket = "${aws_s3_bucket.zappa_and_secrets.bucket}"
+  key    = "production_secrets.json"
 
   content = <<JSON
 {
   "DATABASE_HOST": "${aws_db_instance.default.address}",
   "DATABASE_NAME": "${postgresql_database.electionleaflets_production.name}",
   "DATABASE_USER": "${postgresql_role.electionleaflets_production.name}",
-  "DATABASE_PASS": "${random_id.db_production_password.b64}",
+  "DATABASE_PASS": "${random_id.db_production_password.b64}"
 }
   JSON
 }
