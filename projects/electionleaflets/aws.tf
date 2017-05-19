d0b88de7110958f34c3ceda4337244c179be8207
Paul Bowsher <paul.bowsher@gmail.com>
2017-05-19 17:48:22 +0100

    Initial setup of electionleaflets project

---
+++ projects/electionleaflets/aws.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket     = "democracyclub-terraform-state"
+    key        = "electionleaflets.tfstate"
+    region     = "eu-west-2"
+    kms_key_id = "99a1dc31-2358-4936-915a-ad96e07ac0de"
+    encrypt    = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_region" "current" {
+  current = true
+}
