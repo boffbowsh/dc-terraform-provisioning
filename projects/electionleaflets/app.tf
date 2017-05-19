fe0b65cc6569db986c45ba21ec2844bd86cde29d
Paul Bowsher <paul.bowsher@gmail.com>
2017-05-19 17:56:52 +0100

    Store the secrets in S3
    Also provision a KMS key to encrypt with

---
+++ projects/electionleaflets/app.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "zappa_and_secrets" {
+  bucket = "zappa-electionleaflets-${data.aws_region.current.name}"
+  acl    = "private"
+  region = "eu-west-1"
+}
+
+resource "aws_kms_key" "secrets_key" {
+  description = "Election Leaflets Zappa secrets key"
+}
+
+resource "aws_s3_bucket_object" "staging_secrets" {
+  bucket     = "${aws_s3_bucket.zappa_and_secrets.bucket}"
+  key        = "staging_secrets.json"
+  kms_key_id = "${aws_kms_key.secrets_key.arn}"
+
+  content = <<JSON
+{
+  "DATABASE_HOST": "${aws_db_instance.default.address}",
+  "DATABASE_NAME": "${postgresql_database.electionleaflets_staging.name}",
+  "DATABASE_USER": "${postgresql_role.electionleaflets_staging.name}",
+  "DATABASE_PASS": "${random_id.db_staging_password.b64}",
+}
+  JSON
+}
+
+resource "aws_s3_bucket_object" "production_secrets" {
+  bucket     = "${aws_s3_bucket.zappa_and_secrets.bucket}"
+  key        = "production_secrets.json"
+  kms_key_id = "${aws_kms_key.secrets_key.arn}"
+
+  content = <<JSON
+{
+  "DATABASE_HOST": "${aws_db_instance.default.address}",
+  "DATABASE_NAME": "${postgresql_database.electionleaflets_production.name}",
+  "DATABASE_USER": "${postgresql_role.electionleaflets_production.name}",
+  "DATABASE_PASS": "${random_id.db_production_password.b64}",
+}
+  JSON
+}
